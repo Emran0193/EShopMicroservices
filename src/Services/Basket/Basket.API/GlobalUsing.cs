c2b357bf3bbdf1ef212cbda50ed618cd780addaf
Emran Husain <emran.huasin@gmail.com>
2024-10-25 13:40:02 +0530

    Basket api

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,12 @@
+﻿global using Basket.API.Models;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using Marten;
+global using Basket.API.Data;
+global using Basket.API.Exceptions;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
+global using BuildingBlocks.Exceptions;
