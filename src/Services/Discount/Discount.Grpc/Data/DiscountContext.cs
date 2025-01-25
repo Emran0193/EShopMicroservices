a720bba8cf489685f38aa068129e016485bb2a1d
Emran Husain <emran.huasin@gmail.com>
2025-01-25 11:48:03 +0530

    Setting up grpc services for discount.

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
+    {
+        public DbSet<CouponModel> Coupons { get; set; }
+    }
+}
